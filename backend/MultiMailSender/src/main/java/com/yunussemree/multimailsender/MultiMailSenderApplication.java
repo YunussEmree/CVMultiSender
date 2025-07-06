62bf4caae0f2172f2fe703bab627b5b202006ec1
YunussEmree <senyigityunusemre@gmail.com>
2025-07-06 12:54:51 +0300

    Initial backend

---
+++ backend/MultiMailSender/src/main/java/com/yunussemree/multimailsender/MultiMailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.yunussemree.multimailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiMailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiMailSenderApplication.class, args);
+    }
+
+}
---
+++ backend/MultiMailSender/src/test/java/com/yunussemree/multimailsender/MultiMailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunussemree.multimailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiMailSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
